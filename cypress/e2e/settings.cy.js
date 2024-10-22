12fb512a673687925dd3945ea66f1e1ecbb59075
luckezQA <labchuk2007@gmail.com>
2024-10-22 16:39:18 +0300

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,60 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+import HomePageObject from '../support/pages/home.pageObject';
 
-  });
+const { faker } = require('@faker-js/faker');
+const { register } = require('module');
+
+describe('Settings page', () => {
+  const homePage = new HomePageObject();
+  const username = faker.word.noun();
+  const email = `${username}@gmail.com`;
+  const password = '123qwe123';
+  const newPasswrod = '123qwe321';
+  const updateUsername = `${username}1`;
+  const bio = faker.word.words(2);
+  const updateBio = `${bio} ${username}`;
+  const updateEmail = `${updateUsername}@gmail.com`;
+
+  Cypress.on('uncaught:exception', (err, runnable) => {   
+    return false; 
+});  
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.register(email, username, password, bio);
+    cy.login(email, username, password);
+    cy.visit('/');
   });
 
   it('should provide an ability to update username', () => {
-
+    homePage.clickOnSettingsLink();
+    homePage.fillUpdateSettingUsername(updateUsername);
+    homePage.clickOnUpdateSettingsBtn();
+    homePage.veifyUpdateUsername(updateUsername);
   });
 
   it('should provide an ability to update bio', () => {
-
+    homePage.clickOnSettingsLink();
+    homePage.fillUpdateSettingsBio(updateBio);
+    homePage.clickOnUpdateSettingsBtn();
+    homePage.verifyUpdateBio(updateBio);
   });
 
   it('should provide an ability to update an email', () => {
+    homePage.clickOnSettingsLink();
+    homePage.fillUpdateSettingsEmail(updateEmail);
+    homePage.clickOnUpdateSettingsBtn();
+    homePage.clickOnSettingsLink();
+    homePage.verifyUpdateEmail(updateEmail);
 
   });
 
   it('should provide an ability to update password', () => {
-
+    homePage.clickOnSettingsLink();
+    homePage.fillUpdateSettingPassword(newPasswrod);
+    homePage.clickOnUpdateSettingsBtn();
   });
 
-  it('should provide an ability to log out', () => {
-
-  });
 });
--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -6,6 +6,73 @@ class HomePageObject extends PageObject {
   get usernameLink() {
     return cy.getByDataCy('profile-link');
   }
+
+  assertHeaderContainUsername(username) {
+    this.usernameLink
+      .should('contain', username);
+  }
+  get settingsLink() {
+    return cy.getByDataCy('user-setting-link');
+  }
+  clickOnSettingsLink() {
+    this.settingsLink.click();
+  }
+
+  get settingsUsernameFiled() {
+    return cy.getByDataCy('username-input-field');
+  }
+
+  fillUpdateSettingUsername(updateUsername) {
+    this.settingsUsernameFiled.should('not.be.disabled')
+      .clear().type(updateUsername);
+  }
+
+  get updateSettingsBtn() {
+    return cy.getByDataCy('update-settings-button');
+  }
+
+  clickOnUpdateSettingsBtn() {
+    this.updateSettingsBtn.click();
+  }
+
+  veifyUpdateUsername(updateUsername) {
+    this.usernameLink.should('contain', updateUsername);
+  }
+  get settingsBioField() {
+    return cy.getByDataCy('bio-input-field');
+  }
+
+  fillUpdateSettingsBio(updateBio) {
+    this.settingsBioField.clear().type(updateBio);
+  }
+  get accountBioField() {
+    return cy.getByDataCy('account-bio-field');
+  }
+
+  verifyUpdateBio(updateBio) {
+    this.accountBioField.should('contain', updateBio);
+  }
+
+  get settingsEmailFiled() {
+    return cy.getByDataCy('email-input-field');
+  }
+
+  fillUpdateSettingsEmail(updateEmail) {
+    this.settingsEmailFiled.clear().type(updateEmail);
+  }
+
+  verifyUpdateEmail(updateEmail) {
+    this.settingsEmailFiled.should('contain.value', updateEmail);
+  }
+
+  get newPasswrodField() {
+    return cy.getByDataCy('newpassword-input-field');
+  }
+
+  fillUpdateSettingPassword(newPasswrod) {
+    this.newPasswrodField.type(newPasswrod);
+  }
+
 }
 
 export default HomePageObject;
