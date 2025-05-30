3b998251a4f0b407db5d8af437f6ccdb8f83a891
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-05-30 15:46:36 +0300

    e2e_settings_pom_local

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,66 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+import SignInPageObject from '../support/pages/signIn.pageObject';
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import { faker } from '@faker-js/faker';
 
-  });
+const signInPage = new SignInPageObject();
+const settingsPage = new SettingsPageObject();
+
+const newData = {
+  newName: faker.internet.userName().replace(/[^a-zA-Z0-9_]/g, ''),
+  newBio: faker.lorem.text(),
+  newEmail: faker.internet.email(),
+  newPassword: faker.internet.password(),
+};
+
+describe('Settings page', () => {
+  let user;
 
   beforeEach(() => {
+    cy.task('db:clear');
 
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+
+      signInPage.visit();
+      cy.login(user.email, user.username, user.password);
+
+      signInPage.typeEmail(user.email);
+      signInPage.typePassword(user.password);
+      signInPage.clickSignInBtn();
+
+      settingsPage.visit();
+    });
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.userNameField.should('have.value', user.username);
+    settingsPage.userNameField.clear();
+    settingsPage.typeUserName(newData.newName);
+    settingsPage.userNameField.should('have.value', newData.newName);
+    settingsPage.updateSettings();
   });
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.bioField.should('have.value', '');
+    settingsPage.typeBio(newData.newBio);
+    settingsPage.bioField.should('contain.text', newData.newBio);
+    settingsPage.updateSettings();
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.emailField.should('have.value', user.email);
+    settingsPage.emailField.clear();
+    settingsPage.typeEmail(newData.newEmail);
+    settingsPage.emailField.should('have.value', newData.newEmail);
+    settingsPage.updateSettings();
   });
 
   it('should provide an ability to update password', () => {
-
-  });
-
-  it('should provide an ability to log out', () => {
-
+    settingsPage.typePassword(newData.newPassword);
+    settingsPage.passwordField.should('have.value', newData.newPassword);
+    settingsPage.updateSettings();
   });
 });
