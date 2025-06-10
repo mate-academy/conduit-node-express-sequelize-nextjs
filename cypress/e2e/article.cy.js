a32ad0bc1f53db47c29257a8a634f7c6586b4a0f
Csillag61 <csillagok@gmail.com>
2025-06-09 20:33:15 -0400

    settings_pom_local

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -15,22 +15,34 @@ describe('Article Management', () => {
   })
 
   it('should be created using New Article form', () => {
-    articlePage.createArticle({
-      title: faker.lorem.words(5),
-      description: faker.lorem.sentence(),
-      body: faker.lorem.paragraph(),
-    })
+    const title = faker.lorem.words(5);
+    const description = faker.lorem.sentence();
+    const body = faker.lorem.paragraph();
 
-    articlePage.assertArticleExists()
+    articlePage.createArticle({ title, description, body });
+    cy.wait(2000); // Allow time for article creation
+
+    articlePage.assertArticleExists(title);
+    cy.log('Article successfully created:', title);
   })
 
   it('should be edited using CustomLink', () => {
-    articlePage.editArticle(faker.lorem.words(6))
-    articlePage.assertArticleUpdated()
+    const newTitle = faker.lorem.words(6);
+
+    cy.get('.article-list').should('contain', newTitle); // Ensure article exists before editing
+    articlePage.editArticle(newTitle);
+    cy.wait(2000); // Allow UI updates before asserting
+
+    articlePage.assertArticleUpdated(newTitle);
+    cy.log('Article successfully edited:', newTitle);
   })
 
   it('should be deleted using Delete button', () => {
-    articlePage.deleteArticle()
-    articlePage.assertArticleDeleted()
+    cy.get('.article-list').should('exist'); // Ensure article exists before deleting
+    articlePage.deleteArticle();
+    cy.wait(2000); // Allow UI time to remove the article
+
+    articlePage.assertArticleDeleted();
+    cy.log('Article successfully deleted');
   })
 })
--- cypress/support/pages/article.pageObject.js
+++ cypress/support/pages/article.pageObject.js
@@ -3,36 +3,69 @@ import PageObject from '../PageObject';
 class ArticlePageObject extends PageObject {
   visit() {
     cy.visit('/editor');
+    cy.url().should('include', '/editor');
   }
 
   createArticle({ title, description, body }) {
-    cy.get('[data-cy=title]').type(title);
-    cy.get('[data-cy=description]').type(description);
-    cy.get('[data-cy=body]').type(body);
-    cy.get('[data-cy=publish]').click();
+    cy.get('[data-cy=title]').should('be.visible').type(title);
+    cy.get('[data-cy=description]').should('be.visible').type(description);
+    cy.get('[data-cy=body]').should('be.visible').type(body);
+    cy.get('[data-cy=publish]').should('be.enabled').click();
+
+    // Wait for the article to appear in the list as confirmation of creation
+    cy.get('.article-list').should('contain', title);
+
+    cy.log('Created article with title:', title);
   }
 
-  editArticle() {
-    cy.get('[data-cy=edit-article]').click();
+  editArticle(newTitle) {
+    cy.get('[data-cy=edit-article]')
+      .should('exist')
+      .click();
+
     cy.get('[data-cy=title]').clear();
-    cy.get('[data-cy=title]').type('Your new title here');
-    cy.contains('Save Changes').click();
+    cy.get('[data-cy=title]').type(newTitle);
+    cy.contains('Save Changes').should('be.enabled').click();
+
+    // Wait for the updated title to appear in the article list
+    cy.get('.article-list').should('contain', newTitle);
+
+    cy.log('Edited article to:', newTitle);
   }
 
   deleteArticle() {
-    cy.getByDataCy('delete-article').click();
+    cy.get('[data-cy^="delete-article"]')
+      .should('exist')
+      .click();
+
+    // Optionally, confirm the article is removed from the list
+    // Example: cy.get('.article-list').should('not.contain', <articleTitle>);
+
+    cy.log('Deleted the article');
   }
 
   assertArticleExists(title) {
-    cy.get('.article-list').should('contain', title);
+    cy.get('.article-list')
+      .should('be.visible')
+      .and('contain', title);
+
+    cy.log('Confirmed article exists:', title);
   }
 
   assertArticleUpdated(newTitle) {
-    cy.get('.article-list').should('contain', newTitle);
+    cy.get('.article-list')
+      .should('be.visible')
+      .and('contain', newTitle);
+
+    cy.log('Confirmed article update:', newTitle);
   }
 
   assertArticleDeleted(title) {
-    cy.get('.article-list').should('not.contain', title);
+    cy.get('.article-list')
+      .should('be.visible')
+      .and('not.contain', title);
+
+    cy.log('Confirmed article deletion:', title);
   }
 }
 
