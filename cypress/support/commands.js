5f436ed2509fb88309c30410dd6ab451c9ebb3fc
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-25 09:13:57 +0300

    commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,7 +28,8 @@
 
 Cypress.Commands.add('getByDataCy', (selector) => {
   cy.get(`[data-cy^="${selector}"]`);
-});
+})
+
 
 Cypress.Commands.add('register', (email = 'riot@qa.team', username = 'riot', password = '12345Qwert!') => {
   cy.request('POST', '/api/users', {
@@ -40,12 +41,11 @@ Cypress.Commands.add('register', (email = 'riot@qa.team', username = 'riot', pas
   });
 });
 
-Cypress.Commands.add('login', (email = 'riot@qa.team', username = 'riot', password = '12345Qwert!') => {
-  cy.request('POST', '/api/users', {
+Cypress.Commands.add('login', (email = 'riot@qa.team', password = '12345Qwert!') => {
+  cy.request('POST', '/api/users/login', {
     user: {
       email,
-      username,
-      password
+    password
     }
   }).then(response => {
     const user = {
