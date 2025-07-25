f2ce4c0edbdca31ab680f9262e4e6faf3dc1679d
Heny <jackbalageru@yahoo.com>
2025-07-25 11:11:56 +0300

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,72 @@
-/// <reference types="cypress" />
-/// <reference types="../support" />
+/// <reference types='cypress' />
+/// <reference types='../support' />
 
-describe('Settings page', () => {
-  before(() => {
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
 
-  });
+const settingsPage = new SettingsPageObject();
+const signInPage = new SignInPageObject();
+const homePage = new HomePageObject();
+
+describe('User Settings page', () => {
+  let user;
+  let newUserData = {};
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+      cy.login(user.email, user.username, user.password);
+      settingsPage.visit();
+    });
   });
 
-  it('should provide an ability to update username', () => {
-
+  it('should allow updating the bio', () => {
+    cy.task('generateRandomString', 10).then((newBio) => {
+      newUserData.bio = newBio;
+      settingsPage.typeBio(newUserData.bio);
+      settingsPage.updateSettings();
+      settingsPage.visit();
+      settingsPage.bioField.should('have.value', newUserData.bio);
+    });
   });
 
-  it('should provide an ability to update bio', () => {
-
+  it('should allow updating the username', () => {
+    cy.task('generateUser').then((generatedNewUser) => {
+      newUserData.username = generatedNewUser.username;
+      settingsPage.typeUserName(newUserData.username);
+      settingsPage.updateSettings();
+      homePage.visit();
+      homePage.assertHeaderContainUsername(newUserData.username);
+    });
   });
 
-  it('should provide an ability to update an email', () => {
-
+  it('should allow updating the email', () => {
+    cy.task('generateUser').then((generatedNewUser) => {
+      newUserData.email = generatedNewUser.email;
+      settingsPage.typeEmail(newUserData.email);
+      settingsPage.updateSettings();
+      settingsPage.logout();
+      signInPage.visit();
+      signInPage.typeEmail(newUserData.email);
+      signInPage.typePassword(user.password);
+      signInPage.clickSignInBtn();
+      homePage.assertHeaderContainUsername(user.username);
+    });
   });
 
-  it('should provide an ability to update password', () => {
-
-  });
-
-  it('should provide an ability to log out', () => {
-
+  it('should allow updating the password', () => {
+    cy.task('generateUser').then((generatedNewUser) => {
+      newUserData.password = generatedNewUser.password;
+      settingsPage.typePassword(newUserData.password);
+      settingsPage.updateSettings();
+      settingsPage.logout();
+      signInPage.visit();
+      signInPage.typeEmail(user.email);
+      signInPage.typePassword(newUserData.password);
+      signInPage.clickSignInBtn();
+      homePage.assertHeaderContainUsername(user.username);
+    });
   });
 });
