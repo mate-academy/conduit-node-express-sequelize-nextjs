1480d9b4402281a44325f58b1ef558681b2c7315
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-28 18:46:10 +0200

    Update home.pageObject.js

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -1,29 +1,16 @@
-export class SettingsPage {
-  visit() {
-    cy.visit('/settings');
+import PageObject from '../PageObject';
+
+class HomePageObject extends PageObject {
+  url = '/#/';
+
+  get usernameLink() {
+    return cy.getByDataCy('profile-link');
   }
 
-  getUsernameInput() {
-    return cy.get('input[placeholder="Username"]');
-  }
-
-  getEmailInput() {
-    return cy.get('input[placeholder="Email"]');
-  }
-
-  getPasswordInput() {
-    return cy.get('input[placeholder="New Password"]');
-  }
-
-  getBioTextarea() {
-    return cy.get('textarea[placeholder="Short bio about you"]');
-  }
-
-  getUpdateButton() {
-    return cy.contains('button', 'Update Settings');
-  }
-
-  getLogoutButton() {
-    return cy.contains('button', 'Or click here to logout.');
+  assertHeaderContainUsername(username) {
+    this.usernameLink
+      .should('contain', username);
   }
 }
+
+export default HomePageObject;
