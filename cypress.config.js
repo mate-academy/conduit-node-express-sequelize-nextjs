673eba81a9be577545f88f9f0b6e34f5ac283baf
Natalia <nataliia.topornitska@gmail.com>
2025-08-31 20:23:14 +0300

    add task solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'cypress';
-import { faker } from '@faker-js/faker';
-import { clear } from './dataBase';
+/* eslint-disable semi */
+import { defineConfig } from 'cypress'
+const faker = require('faker')
+const { clear } = require('./dataBase')
 
 module.exports = defineConfig({
   e2e: {
@@ -8,27 +9,27 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
-          let randomNumber = Math.ceil(Math.random(1000) * 1000);
-          let userName = faker.name.firstName() + `${randomNumber}`;
+          let randomNumber = Math.ceil(Math.random(1000) * 1000)
+          let userName = faker.name.firstName() + `${randomNumber}`
           return {
             username: userName.toLowerCase(),
-            email: 'test'+`${randomNumber}`+'@mail.com',
+            email: 'test' + `${randomNumber}` + '@mail.com',
             password: '12345Qwert!',
-          };
+          }
         },
         generateArticle() {
           return {
             title: faker.lorem.word(),
             description: faker.lorem.words(),
             body: faker.lorem.words(),
-            tag: faker.lorem.word()
-          };;
+            tag: faker.lorem.word(),
+          }
         },
         'db:clear'() {
-          clear();
-          return null;
+          clear()
+          return null
         },
-      });
+      })
     },
   },
-});
+})
