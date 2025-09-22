c5b2b54588727ad62280176025865da6baed7e35
Adam <adamur.ichigo@onet.pl>
2025-09-22 13:10:52 +0200

    Add E2E tests for profile updates

--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -1,7 +1,11 @@
 class PageObject {
-  visit(url) {
-    cy.visit(url || this.url);
-  }
+    visit(url) {
+        cy.visit(url);
+    }
+
+    getByDataCy(selector) {
+        return cy.get(`[data-cy=${selector}]`);
+    }
 }
 
 export default PageObject;
