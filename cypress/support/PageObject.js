59e4fd68083d485ca411abc3966e324d9b459a77
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 10:47:39 +0200

    chore(pom): update PageObject.js with alert helper

--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -2,6 +2,12 @@ class PageObject {
   visit(url) {
     cy.visit(url || this.url);
   }
+
+  assertAlert(alertMessage) {
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.eq(alertMessage);
+    });
+  }
 }
 
 export default PageObject;
